7f50d33546ae63e93c501454f40dfd55dcda8731
Samie Mostafavi <samiemostafavi@gmail.com>
2023-01-23 18:21:26 +0100

    Update scan-internal.py

--- scan-internal.py
+++ scan-internal.py
@@ -65,7 +65,7 @@ for host in hosts:
     remoteServer = host                                                  
     port = hosts[host]['port']                                           
                                                                          
-    print "-" * 90                                                       
+    print("-" * 90)
                                                                          
     # We also put in some error handling for catching errors             
     try:                                                                 
@@ -80,14 +80,14 @@ for host in hosts:
             resStr = 'Down'                                              
                                                                          
     except KeyboardInterrupt:                                            
-        print "You pressed Ctrl+C"                                       
+        print("You pressed Ctrl+C")
         sys.exit()        
     except socket.gaierror:                                              
         remoteServerIP = '-'                                             
         resStr = 'Hostname not found'                                    
                                                                          
     except socket.error:                                                 
-        print "Couldn't connect to server"                               
+        print("Couldn't connect to server")
         sys.exit()                                                       
                                                                          
     print("{:<30} {:<20} {:<10} {:<20}".format(remoteServer,remoteServerIP,port,resStr))
